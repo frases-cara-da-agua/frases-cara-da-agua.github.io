86f017f6116eaee37ba271600d2dc56aadeec925
frases-cara-da-agua <111146587+frases-cara-da-agua@users.noreply.github.com>
2022-08-12 14:57:37 -0300

    iniciando a bagaceira

---
+++ freseador.js
@@ -0,0 +1,31 @@
+const proxy = 'http://alloworigin.com/get?url=';
+const url = `${proxy}https://positive-vibes-api.herokuapp.com/quotes/random`;
+//'';
+
+fetch(url,{
+    method: 'GET',
+    mode: 'cors',
+    headers: new Headers({
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type':  'application/json',
+        'dataType': 'jsonp'
+    })
+}).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    console.log(data);
+  }).catch(function() {
+    console.log("Booo");
+  });
+
+  function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+ console.log(bgColor);
+  
+    document.body.style.background = bgColor;
+    }
+
+random_bg_color();
