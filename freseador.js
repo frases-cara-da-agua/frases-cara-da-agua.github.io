215902eebf4fc86d3d18111fb518ccffc185d180
frases-cara-da-agua <111146587+frases-cara-da-agua@users.noreply.github.com>
2022-08-12 17:48:49 -0300

    mudei a api pq a outra tava dificir :)

--- freseador.js
+++ freseador.js
@@ -1,17 +1,35 @@
-const url = `https://positive-vibes-api.herokuapp.com/quotes/random`;
-//'';
+const url = `http://allugofrases.herokuapp.com/frases/random`;
+const head_opt = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Cache-Control':' max-age=0',
+    'Connection': 'keep-alive',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode':' navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+    'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': "Windows"
+};
+const headers = new Headers(head_opt);
+var div_frase = document.querySelector(".frase")
 
 fetch(url,{
     method: 'GET',
-    headers: new Headers({
-        'Content-Type':  'application/json'
-    })
+    //mode: 'no-cors',
+    headers: headers,
 }).then(function(response) {
     return response.json();
   }).then(function(data) {
     console.log(data);
+    div_frase.innerHTML = data.frase;
+
   }).catch(function() {
-    console.log("Booo");
+    console.log("oi");
   });
 
   function random_bg_color() {
